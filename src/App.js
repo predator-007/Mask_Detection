cc33c5f7d3263ffd42e99098f649c4c057dbb458
predator-007 <srijanraj2000@gmail.com>
2021-06-03 11:58:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -16,25 +16,18 @@ function App() {
   const clas=['with mask','without mask']
         
   const [image,setimage]=useState(null);
+  const [cmode,setcmode]=useState(true);
   var Model;
   var imgtensor;
-  const loadmodel=async()=>{
-  Model=await tfjs.loadLayersModel("/mymodeljs/model.json");
-  console.log("model loaded");
-  }
   const captureimg = React.useCallback(
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
       setimage(imageSrc);
-      console.log(image);
-
+      setcmode(false);
     },
 
     [webcamRef]
   );
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
 
   const stoi=(a)=>{
     var index=parseInt(a[0]);
@@ -72,19 +65,18 @@ function App() {
   }
   return (
     <div className="App" >
-     <h1
-            style={
-                {
-                    display:"block",
-                    textAlign:"center",
-                    color:"darkred",
-                    textShadow:"5px 5px 10px #00FF00",
-                    
-                }
-            }>
-      mask detection</h1>
+      <pre className="titleborder">
+     <h1 className="title">
+      Mask Detection</h1></pre>
+    {
+      cmode?
+    
     <div className="cam" 
+    style={{
+      display:"block"
+    }}
     >
+    
     <Webcam
         audio={false}
         height={400}
@@ -92,47 +84,95 @@ function App() {
         screenshotFormat="image/jpeg"
         width={460}
         videoConstraints={videoConstraints}
-        style={{marginRight:"10px"}}
-      />
-    <img className="capturedimg" src={image} width="260" height="200"
-    style={{
-      marginBottom:"8%"
-    }}
-    ></img>
-    </div>
+        style={{display:"block",
+              marginLeft:"auto",
+              marginRight:"auto",
+      }}
+        
+    />
+    
     <Button
     variant="contained"
     color="primary"
+    style={{
+      display:"block",
+      marginLeft:"auto",
+      marginRight:"auto",
+      fontFamily:"sans-serif"
+    }}
     onClick={(e)=>{e.preventDefault();captureimg();}}
-    >capture</Button>
+    > <i class="fas fa-camera"></i> capture</Button>
+    
+    </div>
+    :
+    <div className="capturedmode">
+    <img className="capturedimg" src={image} width="260" height="280"
+    style={{
+      display:"block",
+      marginLeft:"auto",
+      marginRight:"auto"
+    }}
+    ></img>
+    <div className="buttons"
+    style={
+      {
+        marginTop:"2%",
+        display:"flex",
+        justifyContent:"center",
+      }
+    }
+    >
+    <Button
+    variant="contained"
+    color="primary"
+    style={{
+        marginRight:"6%",
+    }} 
+    onClick={()=>setcmode(true)}
+    ><i class="fas fa-redo"> Retake</i></Button>
+    
     <Button
     variant="contained"
     color="secondary"
-    style={{marginLeft:"10%"}} 
+    style={{
+    
+    }} 
     onClick={()=>pred()}
-    >predict</Button>
+    >Predict</Button>
+    </div>
     {
       loading?
       <div>
-      <img src={"https://wpamelia.com/wp-content/uploads/2018/11/ezgif-2-6d0b072c3d3f.gif"}
+      <img src={"https://acegif.com/wp-content/uploads/loading-42.gif"}
       style={
         {
           width:"15%",
           height:"10%",
-          display:"inline-block",
+          display:"block",
           marginLeft:"auto",
-          marginRight:"auto",
+          marginRight:"auto"
         }
       }
       ></img>
       </div>
       :(prediction=="with mask" )?
-      <h2 style={{color:"green",boxShadow:"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"}}>{prediction}</h2>
+      <h2 style={{
+        marginTop:"2%",
+        color:"darkblue",
+        textAlign:"center",
+        animation:"shrink 5s linear infinite"
+        }}>{prediction}</h2>
       :
-      <h2 style={{color:"red",boxShadow:"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"}}>{prediction}</h2>
+      <h2 style={{
+        marginTop:"2%",
+        color:"darkblue",
+        textAlign:"center",
+        animation:"shrink 5s linear infinite"
+      }}>{prediction}</h2>
     }
-    
-    
+    </div>
+
+    }    
     </div>
         
   );
